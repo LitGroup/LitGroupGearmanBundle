6b58e2fa1f5502a2e16396a4f92abc109f45550f
Roman Shamritskiy <roman@litgroup.ru>
2013-07-22 00:47:43 +0400

    Update file-level docblocks

--- DependencyInjection/LitGroupGearmanExtension.php
+++ DependencyInjection/LitGroupGearmanExtension.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * This file is part of the LitGroup JobQueue package.
+ * This file is part of the LitGroupGearmanBundle package.
  *
- * @copyright Roman Shamritskiy <roman@litgroup.ru>
+ * (c) Roman Shamritskiy <roman@litgroup.ru>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- Tests/DependencyInjection/AbstractLitGroupGearmanExtensionTest.php
+++ Tests/DependencyInjection/AbstractLitGroupGearmanExtensionTest.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of the LitGroupGearmanBundle package.
+ *
+ * (c) Roman Shamritskiy <roman@litgroup.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace LitGroup\Bundle\GearmanBundle\Tests\DependencyInjection;
 
